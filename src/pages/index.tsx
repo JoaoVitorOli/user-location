9c0e06beec3185dad87c820b57dac9d0861c83a1
João Vitor <infamoz_br3@hotmail.com>
2022-03-14 18:16:42 -0300

    finalizado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  const [clientCountryCode, setClientCountryCode] = useState("");
+  const [clientCountryCode, setClientCountryCode] = useState("br");
   
   useEffect(() => {
     async function test() {
@@ -30,13 +30,27 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1>code: {clientCountryCode && clientCountryCode}</h1>
+        <h1 style={{color: "#d3d3d3"}}>code: {clientCountryCode && clientCountryCode}</h1>
         
         <Image
           src={`https://countryflagsapi.com/png/${clientCountryCode}`}
-          width={240}
+          width={300}
           height={200}
         />
+
+        {clientCountryCode !== "jp" && (
+          <div>
+            <h3 style={{color: "#d3d3d3"}}>email: brasil@email.com</h3>
+            <h3 style={{color: "#d3d3d3"}}>telefone: (99) 9999-9999</h3>
+          </div>
+        )}
+        
+        {clientCountryCode === "jp" && (
+          <div>
+            <h3 style={{color: "#d3d3d3"}}>email: japao@email.com</h3>
+            <h3 style={{color: "#d3d3d3"}}>telefone: (22) 2222-2222</h3>
+          </div>
+        )}
       </main>
     </div>
   )
