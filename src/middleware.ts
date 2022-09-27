53f3e85a5e35d7b93ed568e0ffa45ba0ba8cdd51
João Vitor <infamoz_br3@hotmail.com>
2022-09-27 16:52:49 -0300

    ajusta url do middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,13 +3,13 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 const AUTHORIZED_URLS = [
-  'https://user-location-xi.vercel.app'
+  'https://user-location-xi.vercel.app/api/clientCountry'
 ]
 
 export function middleware(req: NextRequest) {
-  console.log(req.nextUrl);
-
   if (req.url === AUTHORIZED_URLS[0] || req.url === AUTHORIZED_URLS[1]) {
+    console.log(req.url);
+
     const response = NextResponse;
 
     const country = req.geo?.country;
