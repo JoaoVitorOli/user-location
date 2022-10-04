88d5100d7c916235a9829f8bd02d7242cde022d3
João Vitor <infamoz_br3@hotmail.com>
2022-10-04 10:40:50 -0300

    testa middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,13 +5,13 @@ import type { NextRequest } from 'next/server'
 export function middleware(req: NextRequest) {
   if (req.nextUrl.pathname === '/api/clientCountry') {
     const country = req.geo?.country || 'BR';
-    
+
     if (country === 'JP') {
       let urlFrom = req.nextUrl.searchParams.get('from');
 
       req.nextUrl.searchParams.set('c', country);
 
-      urlFrom = `?c=${country}`
+      urlFrom = urlFrom + `?c=${country}`;
 
       return NextResponse.redirect(urlFrom);
     }
