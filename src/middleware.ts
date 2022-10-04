11ed9ffd801b832dcd740822d8870963e4e5ec5f
João Vitor <infamoz_br3@hotmail.com>
2022-10-04 09:40:42 -0300

    testa middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,18 +2,14 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-const AUTHORIZED_URLS = [
-  'https://user-location-xi.vercel.app/api/clientCountry'
-]
-
 export function middleware(req: NextRequest) {
-  if (req.nextUrl.pathname === AUTHORIZED_URLS[0]) {
-    console.log(req.url);
+  if (req.nextUrl.pathname === '/api/clientCountry') {
+    const country = req.geo?.country || 'BR';
 
-    const country = req.geo?.country;
+    if (country === 'JP') {
+      req.nextUrl.searchParams.set('c', country!);
 
-    req.nextUrl.searchParams.set('c', country!);
-
-    return NextResponse.redirect(req.nextUrl);
+      return NextResponse.redirect(req.nextUrl);
+    }
   }
 }
